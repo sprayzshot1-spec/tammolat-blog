c2885537e748fe0caed15f64d696c0c3b2d12ec7
sprayzshot1-spec <sprayzshot1@gmail.com>
2026-07-11 01:43:24 +0300

    Shker

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
 
-// 🔴 تم ربط هذا المتغير بمسار الصورة الجديدة التي قمت بتصميمها للواتساب والفيسبوك
-export const HOME_OG_IMAGE_URL = "/images/og-image.png";
+// 🔴 تم وضع الرابط كاملاً لضمان قراءته في كل المنصات بدون أخطاء دمج
+export const HOME_OG_IMAGE_URL = "https://ahmedshaker.org/images/og-image.png"; 
+// (ملاحظة: إذا حولت الصورة إلى jpg في الخطوة السابقة، لا تنس تغيير الامتداد هنا أيضاً)
