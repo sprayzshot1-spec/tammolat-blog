e4b21a2d1a7d0528caed422900ecd91902d3a3be
sprayzshot1-spec <sprayzshot1@gmail.com>
2026-07-11 03:21:46 +0300

    Shker

--- src/interfaces/post.ts
+++ src/interfaces/post.ts
@@ -1,3 +1,4 @@
+// src/interfaces/post.ts
 import { type Author } from "./author";
 
 export type Post = {
@@ -12,20 +13,5 @@ export type Post = {
   };
   content: string;
   preview?: boolean;
-};
-
-
-export type Post = {
-  slug: string;
-  title: string;
-  date: string;
-  coverImage: string;
-  author: Author;
-  excerpt: string;
-  ogImage: {
-    url: string;
-  };
-  content: string;
-  preview?: boolean;
-  youtubeUrl?: string; // 🔴 إضافة هذا السطر لتعريف رابط اليوتيوب (علامة ؟ تعني أنه اختياري وليس إجبارياً لكل المقالات)
+  youtubeUrl?: string; // 🔴 تم دمج الخاصية هنا في التعريف الوحيد
 };
